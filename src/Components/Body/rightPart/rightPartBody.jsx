c9fef54acaa4533861771c0b81c408e528c549fc
Burhan Ali Shaikh <85751219+Nyx-95@users.noreply.github.com>
2022-12-08 17:56:21 +0500

    update

--- src/Components/Body/rightPart/rightPartBody.jsx
+++ src/Components/Body/rightPart/rightPartBody.jsx
@@ -6,6 +6,11 @@ function rightBodyPart() {
     return (<>
         <Container>
             <div className="rightBodyMain" >
+                <div className='floatingIcons1'>
+                    <div className='floatingIconsImage' >  <i className="ri-image-2-line"></i> <label>Drop.png</label> </div>
+                    <div className='floatingIconsImage1' >  <div className='floatingIconsImageInner'> <i className="ri-music-2-line"></i> </div> </div>
+              
+                </div>
                 <div className="rightBodyMainInner">
                     <h1 className='rightBodyh1'>About</h1>
                     <h1 className='rightBodyh2'>Array Share</h1>
@@ -14,6 +19,16 @@ function rightBodyPart() {
                         support AirDrop.
                     </p>
                 </div>
+                <div className='floatingIcons2'>
+                <div className='floatingIconsImage1' >  <div className='floatingIconsImageInner '> <i className="ri-upload-cloud-line"></i> </div> </div>
+                <div className='floatingIconsImage' >   <i className="ri-folder-shield-2-line"></i> <label>*****</label> </div>
+                
+              
+                </div>
+                <div className='rightBodyMainFooter'>
+                    <label>© 2011-2022 ArrayShare.com  -  Made in <a href='#'>Array Digitals</a></label>
+                </div>
+                
             </div>
         </Container>
     </>);
